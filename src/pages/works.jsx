ed6b3e8ee9937515e85178f69c7b2bf267daf457
Alejandra Sosa <lq.ale@hotmail.com>
2023-03-03 12:28:31 -0600

    08 test

--- src/pages/works.jsx
+++ src/pages/works.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 
 import photoGir from '../assets/gir.png';
 import lll from '../assets/lll.png';
+import cover2D from '../assets/2D/Digital/GirlintheSuburbs.png';
+import cover3D from '../assets/3D/mothbear/mothbear_model01.png';
 import '../style/index.css';
 
 
@@ -15,12 +17,12 @@ export function Works(){
     <div className="wNaviContainer">
       <div className="naviCover">
       <Link to="./Sketchbook" target="_blank" rel="noreferrer">
-        <img src = {photoGir} alt= {lll}></img>
+        <img src = {cover2D} alt= {lll}></img>
       </Link>
       </div>
       <div className="naviCover">
       <Link to="./Sketchbook" target="_blank" rel="noreferrer">
-        <img src = {photoGir} alt= {lll}></img>
+        <img src = {cover3D} alt= {lll}></img>
       </Link>
       </div>
       <div className="naviCover">
